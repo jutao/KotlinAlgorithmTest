29a91d778365fff6ecea662b971ca762c9b8ecc8
jutao <573723112@qq.com>
2018-02-27 23:16:45 +0800

    排序输出与生成随机数

---
+++ src/sort/Main.kt
@@ -0,0 +1,6 @@
+package sort
+
+fun main(args: Array<String>) {
+    var arr:IntArray=SortTestHelper.generateRandomArray(10,3,44)
+    SortTestHelper.printArray(arr)
+}
--- src/sort/SortTestHelper.kt
+++ src/sort/SortTestHelper.kt
@@ -1,4 +1,32 @@
 package sort
 
+import java.util.*
+
 class SortTestHelper {
+    /**
+     * 生成 n 个元素的随机数组，每个元素的范围是 [rangeL,rangeR]
+     */
+    companion object {
+        @JvmStatic
+        fun generateRandomArray(n:Int,rangeL:Int,rangeR:Int):IntArray{
+            assert(rangeL<rangeR)
+            var arr:IntArray= IntArray(n)
+            for(i in 0 until n){
+                arr[i]=Random().nextInt(rangeR-rangeL+1)+rangeL
+            }
+            return arr
+        }
+
+        /**
+         * 输出数组各个元素
+         */
+        @JvmStatic
+        fun printArray(arr:IntArray){
+            for (a in arr) {
+                print(a)
+                print(" ")
+            }
+        }
+    }
+
 }

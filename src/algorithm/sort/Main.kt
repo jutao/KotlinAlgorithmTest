1b1fe0cb2c8ec62716cae610a4927f1ec0dc2ad8
jutao <573723112@qq.com>
2018-11-04 23:22:33 +0800

    归并排序

--- src/algorithm/sort/Main.kt
+++ src/algorithm/sort/Main.kt
@@ -2,9 +2,10 @@ package sort
 
 import algorithm.sort.BubbleSort
 import algorithm.sort.InsertionSort
+import algorithm.sort.MergeSort
 
 fun main(args: Array<String>) {
-    val arr = SortTestHelper.generateRandomArray(1000, 5, 1000)
+    val arr = SortTestHelper.generateRandomArray(500, 5, 100000)
     println("------------------------------------")
     SortTestHelper.printArray(arr)
     //选择法排序
@@ -15,6 +16,7 @@ fun selectionSort(arr: Array<Int>) {
     SortTestHelper.testSort("选择法排序",SelectionSort(),arr,arr.size)
     SortTestHelper.testSort("插入法排序", InsertionSort(),arr,arr.size)
     SortTestHelper.testSort("冒泡法排序", BubbleSort(),arr,arr.size)
+    SortTestHelper.testSort("归并法排序", MergeSort(),arr,arr.size)
 }
 
 
---
+++ src/algorithm/sort/MergeSort.kt
@@ -0,0 +1,80 @@
+package algorithm.sort
+
+import sort.Sort
+
+/**
+ * 归并排序
+ */
+class MergeSort : Sort {
+    override fun <T : Comparable<T>> sort(arr: Array<T>, n: Int) {
+        mergeSort(arr, 0, n - 1)
+    }
+
+    /**
+     * 递归使用归并排序，对 arr[l,r]的范围进行排序
+     */
+    private fun <T : Comparable<T>> mergeSort(arr: Array<T>, l: Int, r: Int) {
+        if (l >= r) {
+            return
+        }
+        //这里有个风险，l+r的值可能会超出 int 的范围
+        var mid = (l + r) / 2
+        mergeSort(arr, l, mid)
+        mergeSort(arr, mid + 1, r)
+        merge(arr, l, mid, r)
+    }
+
+    /**
+     * 将 arr[l,mid] 与 arr[mid+1,r] 两部分进行归并
+     */
+    private fun <T : Comparable<T>> merge(arr: Array<T>, l: Int, mid: Int, r: Int) {
+        val aux = arrayOfNulls<Any>(r - l + 1)
+        for (i in l..r) {
+            aux[i - l] = arr[i]
+        }
+        //左边的数组归并的序号
+        var i = l
+        //右边的数组归并的序号
+        var j = mid + 1
+        for (k in l..r) {
+            when {
+                i > mid -> {
+                    arr[k] = aux[j - l] as T
+                    j++
+                }
+                j > r -> {
+                    arr[k] = aux[i - l] as T
+                    i++
+                }
+                aux[i - l] < aux[j - l] -> {
+                    arr[k] = aux[i - l] as T
+                    i++
+                }
+                else -> {
+                    arr[k] = aux[j - l] as T
+                    j++
+                }
+            }
+        }
+    }
+
+}
+
+private operator fun Any?.compareTo(any: Any?): Int {
+    return try {
+        val a = this as Int
+        val b = any as Int
+        when {
+            a > b -> 1
+            a < b -> -1
+            else -> 0
+        }
+    } catch (exception: Exception) {
+        0
+    }
+
+}
+
+
+
+

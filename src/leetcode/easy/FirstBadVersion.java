2232c9afe1c6e055772af175fa030c1082f78ebb
jutao <jutaoah@163.com>
2019-05-15 14:04:47 +0800

    一道简单题，二分搜索

---
+++ src/leetcode/easy/FirstBadVersion.java
@@ -0,0 +1,39 @@
+package leetcode.easy;
+
+/**
+ * author：jutao
+ * time：2019/5/15
+ * description：278. 第一个错误的版本
+ *
+ * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。
+ * 由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ *
+ * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ *
+ * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。
+ * 实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ */
+public class FirstBadVersion {
+    /**
+     * @param n
+     * @return
+     */
+    public int firstBadVersion(int n) {
+        int l=0,h=n;
+        int m=l+(h-l)/2;
+        while (l<h){
+            if(!isBadVersion(m)){
+                l=m+1;
+            }else {
+                h=m;
+            }
+            m=l+(h-l)/2;
+        }
+        return l;
+    }
+
+    boolean isBadVersion(int version){
+        return version==5;
+    }
+
+}

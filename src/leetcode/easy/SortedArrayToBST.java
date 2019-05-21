0a396c662c91840c2b6953dc6c15bc385dbc1a03
jutao <jutaoah@163.com>
2019-05-21 17:18:04 +0800

    将有序数组转换为二叉搜索树

--- src/leetcode/easy/SortedArrayToBST.java
+++ src/leetcode/easy/SortedArrayToBST.java
@@ -1,34 +1,31 @@
-package easy;
+package leetcode.easy;
 
 /**
  * ****************************************
  * author：琚涛
  * time：2019/1/8
- * description：
+ * description：108. 将有序数组转换为二叉搜索树
+ * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ * <p>
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
  * ****************************************
  */
 public class SortedArrayToBST {
-    public  TreeNode sortedArrayToBST(int[] nums) {
-        if(nums.length==0){
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null) {
             return null;
         }
-        int middle = nums.length / 2;
-        TreeNode root = new TreeNode(nums[middle]);
-        TreeNode currentLeftNode = root;
-        TreeNode currentRightNode = root;
+        return buildTree(nums, 0, nums.length - 1);
+    }
 
-        for (int i = 0; i < middle; i++) {
-            if ((middle - 1 - i) >=0) {
-                currentLeftNode.left = new TreeNode(nums[middle - 1 - i]);
-                currentLeftNode = currentLeftNode.left;
-            }
-            if ((middle + 1 + i) < nums.length) {
-                currentRightNode.right = new TreeNode(nums[middle + 1 + i]);
-                currentRightNode = currentRightNode.right;
-            }
-
+    private TreeNode buildTree(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
         }
-
+        int m = (r - l) / 2 + l;
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = buildTree(nums, l, m - 1);
+        root.right = buildTree(nums, m + 1, r);
         return root;
     }
 
